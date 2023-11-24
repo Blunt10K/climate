85f9a7e5199aba2337d1754be615b0a86f4bbfab
Kenny Burawudi <kburawudi@yahoo.com>
2023-11-25 00:03:01 +0100

    add bmp sensor

--- grove_light_sensor/light.py
+++ grove_light_sensor/light.py
@@ -7,35 +7,36 @@
 import smbus2
 import time
 
-SMBUS_ADDRESS = 1
+BUS = 1
+I2C_ADDRESS = 0x39
 
 # Get I2C bus
 
-with smbus2.SMBus(SMBUS_ADDRESS) as bus:
+with smbus2.SMBus(BUS) as bus:
 
     # TMG39931 address, 0x39(57)
     # Select Enable register, 0x80(128)
     #		0x0F(15)	Power ON, ALS enable, Proximity enable, Wait disable
-    bus.write_byte_data(0x39, 0x80, 0x0F)
+    bus.write_byte_data(I2C_ADDRESS, 0x80, 0x0F)
     # TMG39931 address, 0x39(57)
     # Select ADC integration time register, 0x81(129)
     #		0x00(00)	ATIME : 712ms, Max count = 65535 cycles
-    bus.write_byte_data(0x39, 0x81, 0x00)
+    bus.write_byte_data(I2C_ADDRESS, 0x81, 0x00)
     # TMG39931 address, 0x39(57)
     # Select Wait time register, 0x83(131)
     #		0xFF(255)	WTIME : 2.78ms
-    bus.write_byte_data(0x39, 0x83, 0xFF)
+    bus.write_byte_data(I2C_ADDRESS, 0x83, 0xFF)
     # TMG39931 address, 0x39(57)
     # Select Control register, 0x8F(143)
     #		0x00(00)	AGAIN is 1x
-    bus.write_byte_data(0x39, 0x8F, 0x00)
+    bus.write_byte_data(I2C_ADDRESS, 0x8F, 0x00)
 
     time.sleep(0.8)
 
     # TMG39931 address, 0x39(57)
     # Read data back from 0x94(148), 9 bytes
     # cData LSB, cData MSB, red LSB, red MSB, green LSB, green MSB, blue LSB, blue MSB, proximity
-    data = bus.read_i2c_block_data(0x39, 0x94, 9)
+    data = bus.read_i2c_block_data(I2C_ADDRESS, 0x94, 9)
 
 # Convert the data
 cData = data[1] * 256.0 + data[0]
