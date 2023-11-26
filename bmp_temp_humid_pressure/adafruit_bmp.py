95c9c743a0c7a6032c7afa3cca785f5f435e3fe3
Kenny Burawudi <kburawudi@yahoo.com>
2023-11-26 20:10:29 +0100

    adjust adafruit bmp code

--- bmp_temp_humid_pressure/adafruit_bmp.py
+++ bmp_temp_humid_pressure/adafruit_bmp.py
@@ -1,8 +1,8 @@
 import Adafruit_BMP.BMP085 as BMP085
 
-bmp280 = BMP085.BMP085()
+sensor = BMP085.BMP085()
 
-print(f'Temperature: {bmp280.read_temperature()} C')
-print(f'Temperature: {bmp280.read_pressure()} Pa')
-print(f'Temperature: {bmp280.read_altitude()} metres')
-print(f'Sealevel Pressure: {bmp280.read_sealevel_pressure()}')
+print(f'Temperature: {sensor.read_temperature()} C')
+print(f'Pressure: {sensor.read_pressure()} Pa')
+print(f'Altitude: {sensor.read_altitude()} metres')
+print(f'Sealevel Pressure: {sensor.read_sealevel_pressure()}')
